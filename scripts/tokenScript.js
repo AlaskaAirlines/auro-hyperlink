66a5818f3de240ec27558ecc802594942c0fa047
Dale Sande <dale.sande@alaskaair.com>
2019-05-23 13:45:08 -0700

    initial commit

---
+++ scripts/tokenScript.js
@@ -0,0 +1,40 @@
+console.log('')
+console.log('Build started...');
+
+console.log('')
+console.log('         .         . ')
+console.log('               *       *')
+console.log('')
+console.log('                 * * *')
+console.log('                    !')
+console.log('               *       * ')
+console.log('')
+console.log(" ██████╗ ██████╗ ██╗ ██████╗ ███╗   ██╗")
+console.log("██╔═══██╗██╔══██╗██║██╔═══██╗████╗  ██║")
+console.log("██║   ██║██████╔╝██║██║   ██║██╔██╗ ██║")
+console.log("██║   ██║██╔══██╗██║██║   ██║██║╚██╗██║")
+console.log("╚██████╔╝██║  ██║██║╚██████╔╝██║ ╚████║")
+console.log(" ╚═════╝ ╚═╝  ╚═╝╚═╝ ╚═════╝ ╚═╝  ╚═══╝")
+console.log('') 
+console.log("██████╗ ███████╗███████╗██╗ ██████╗ ███╗   ██╗")
+console.log("██╔══██╗██╔════╝██╔════╝██║██╔════╝ ████╗  ██║")
+console.log("██║  ██║█████╗  ███████╗██║██║  ███╗██╔██╗ ██║")
+console.log("██║  ██║██╔══╝  ╚════██║██║██║   ██║██║╚██╗██║")
+console.log("██████╔╝███████╗███████║██║╚██████╔╝██║ ╚████║")
+console.log("╚═════╝ ╚══════╝╚══════╝╚═╝ ╚═════╝ ╚═╝  ╚═══╝")
+console.log('')
+console.log("███████╗██╗   ██╗███████╗████████╗███████╗███╗   ███╗")
+console.log("██╔════╝╚██╗ ██╔╝██╔════╝╚══██╔══╝██╔════╝████╗ ████║")
+console.log("███████╗ ╚████╔╝ ███████╗   ██║   █████╗  ██╔████╔██║")
+console.log("╚════██║  ╚██╔╝  ╚════██║   ██║   ██╔══╝  ██║╚██╔╝██║")
+console.log("███████║   ██║   ███████║   ██║   ███████╗██║ ╚═╝ ██║")
+console.log("╚══════╝   ╚═╝   ╚══════╝   ╚═╝   ╚══════╝╚═╝     ╚═╝")
+console.log('')                                                 
+
+
+
+// Required dependency
+const tokenConfig = require('style-dictionary').extend('./scripts/tokenConfig.json');
+
+// Style Dictionary build function
+tokenConfig.buildAllPlatforms();
