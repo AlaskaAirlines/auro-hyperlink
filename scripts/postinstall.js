d69fbd15f7c24cdbb9c73aa99c8306d684de11b7
Dale Sande <dale.sande@alaskaair.com>
2020-01-18 12:14:29 -0800

    feat: add support for auro-hyperlink component

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -3,30 +3,33 @@
 const chalk = require('chalk');
 const pjson = require('../package.json');
 
-console.log(chalk.yellow(`
-           _______                   __           __ __
-           |     __|.---.-.--.--.    |  |--.-----.|  |  |.-----.
-           |__     ||  _  |  |  |    |     |  -__||  |  ||  _  |
-           |_______||___._|___  |    |__|__|_____||__|__||_____|
-                          |_____|
-               __              _______                    __
-              |  |_.-----.    |   _   |.--.--.----.-----.|  |
-              |   _|  _  |    |       ||  |  |   _|  _  ||__|
-              |____|_____|    |___|___||_____|__| |_____||__|
+console.log(chalk.hex('#f26135')(`
+ _______                   __           __ __
+|     __|.---.-.--.--.    |  |--.-----.|  |  |.-----.
+|__     ||  _  |  |  |    |     |  -__||  |  ||  _  |
+|_______||___._|___  |    |__|__|_____||__|__||_____|
+               |_____|
+ __              _______                    __
+|  |_.-----.    |   _   |.--.--.----.-----.|  |
+|   _|  _  |    |       ||  |  |   _|  _  ||__|
+|____|_____|    |___|___||_____|__| |_____||__|
 `)
 +
-chalk.yellow(`
-╭ ─ ─ ─ ─ ─ ─ ─ ─ ─ ────────────────────────────────────────────────────────╮
-│                                                                           │
-│     Thanks for installing the latest version of `) + chalk.green.bold(`ods-hyperlink v${pjson.version}.`) + chalk.yellow(`     │
-│     This version begins the transition from Orion to Auro components.     │
-│                                                                           │
-│         Throughout the transition, ods-hyperlink will continue to be      │
-│                FULLY SUPPORTED until no longer needed.                    │
-│                                                                           │
-│         Be sure to see `) +  chalk.green.bold(`https://git.io/Jvk4Z`) + chalk.yellow(` for more information in       │
-│                regards to updates and deprecation strategy.               │
-│                                                                           │
-╰───────────────────────────────────────────────────────── ─ ─ ─ ─ ─ ─ ─ ─ ─╯
+chalk.hex('#f26135')(`
+╭ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ──────────────────────────────╮
+│                                                       │
+│       Thanks for installing the latest version        │
+│               of `) + chalk.hex('#ffd200').bold(`ods-hyperlink v${pjson.version}.`) + chalk.hex('#f26135')(`                │
+│       This version begins the transition from         │
+│               Orion to Auro components.               │
+│                                                       │
+│       Throughout the transition, ods-hyperlink        │
+│         will continue to be FULLY SUPPORTED           │
+│              until no longer required.                │
+│                                                       │
+│   Be sure to see `) +  chalk.hex('#00cff0').bold.underline(`https://git.io/Jvk4Z`) + chalk.hex('#f26135')(` for more info   │
+│          on updates and deprecation strategy.         │
+│                                                       │
+╰─────────────────────────────── ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─╯
 `)
 );
