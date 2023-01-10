1b191787f6e9b58060c736af07584f952616c2ce
Jason Baker <jason@capsule42.com>
2023-01-10 12:44:42 -0800

    feat(customization): allow for CE class extension and customization #151
    Changes to be committed:
    modified:   demo/index.html
    modified:   src/define.js
    deleted:    src/defineCustom.js
    modified:   test/auro-hyperlink.test.js

--- src/defineCustom.js
+++
@@ -1,9 +0,0 @@
-import { AuroHyperlink } from './auro-hyperlink.js';
-
-export class CustomHyperlink extends AuroHyperlink {
-  //
-}
-
-if (!customElements.get("custom-hyperlink")) {
-  customElements.define("custom-hyperlink", CustomHyperlink);
-}
