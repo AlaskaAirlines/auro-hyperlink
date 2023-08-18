c7bb683bd75ab597b35b31317947d34b0a31416a
Dale Sande <dale.sande@gmail.com>
2023-08-18 15:39:54 -0700

    docs: update to latest versions of core docs
    Changes to be committed:
    modified:   .github/CONTRIBUTING.md
    modified:   scripts/generateDocs.js

--- scripts/generateDocs.js
+++ scripts/generateDocs.js
@@ -5,29 +5,26 @@ const https = require('https');
 
 const readmeTemplateUrl = 'https://raw.githubusercontent.com/AlaskaAirlines/WC-Generator/master/componentDocs/README.md';
 const dirDocTemplates = './docTemplates';
-const readmeFilePath = `${dirDocTemplates}/README.md`;
+const readmeFilePath = dirDocTemplates + '/README.md';
 
 /**
- * Extract NPM, NAMESPACE and NAME from package.json.
+ * Extract NPM, NAMESPACE and NAME from package.json
  */
 
-/**
- *
- */
-function nameExtraction() {
-  const packageJson = fs.readFileSync('package.json', 'utf8', (err, data) => {
+ function nameExtraction() {
+  const packageJson = fs.readFileSync('package.json', 'utf8', function(err, data) {
     if (err) {
       console.log('ERROR: Unable to read package.json file', err);
     }
-  });
+  })
 
   pName = JSON.parse(packageJson).name;
 
-  const npmStart = pName.indexOf('@');
-  const namespaceStart = pName.indexOf('/');
-  const nameStart = pName.indexOf('-');
+  let npmStart = pName.indexOf('@');
+  let namespaceStart = pName.indexOf('/');
+  let nameStart = pName.indexOf('-');
 
-  const result = {
+  let result = {
     'npm': pName.substring(npmStart, namespaceStart),
     'namespace': pName.substring(namespaceStart + 1, nameStart),
     'namespaceCap': pName.substring(namespaceStart + 1)[0].toUpperCase() + pName.substring(namespaceStart + 2, nameStart),
@@ -36,21 +33,18 @@ function nameExtraction() {
   };
 
   return result;
-}
+};
 
 /**
- * Replace all instances of [npm], [name], [Name], [namespace] and [Namespace] accordingly.
+ * Replace all instances of [npm], [name], [Name], [namespace] and [Namespace] accordingly
  */
 
-/**
- *
- */
 function formatTemplateFileContents(content, destination) {
-  const nameExtractionData = nameExtraction();
+  let nameExtractionData = nameExtraction();
   let result = content;
 
   /**
-   * Replace placeholder strings.
+   * Replace placeholder strings
    */
   result = result.replace(/\[npm]/g, nameExtractionData.npm);
   result = result.replace(/\[name]/g, nameExtractionData.name);
@@ -59,7 +53,7 @@ function formatTemplateFileContents(content, destination) {
   result = result.replace(/\[Namespace]/g, nameExtractionData.namespaceCap);
 
   /**
-   * Cleanup line breaks.
+   * Cleanup line breaks
    */
   result = result.replace(/(\r\n|\r|\n)[\s]+(\r\n|\r|\n)/g, '\r\n\r\n'); // Replace lines containing only whitespace with a carriage return.
   result = result.replace(/>(\r\n|\r|\n){2,}/g, '>\r\n'); // Remove empty lines directly after a closing html tag.
@@ -68,44 +62,38 @@ function formatTemplateFileContents(content, destination) {
   result = result.replace(/([^(\r\n|\r|\n)])(\r\n|\r|\n)+#/g, "$1\r\n\r\n#"); // Ensure empty line before header sections.
 
   /**
-   * Write the result to the destination file.
+   * Write the result to the destination file
    */
   fs.writeFileSync(destination, result, { encoding: 'utf8'});
-}
+};
 
-/**
- *
- */
 function formatApiTableContents(content, destination) {
   const nameExtractionData = nameExtraction();
-  const wcName = `${nameExtractionData.namespace}-${nameExtractionData.name}`;
+  const wcName = nameExtractionData.namespace + '-' + nameExtractionData.name;
 
   let result = content;
 
-  result = result.
-    replace(/\r\n|\r|\n####\s`([a-zA-Z]*)`/g, `\r\n#### <a name="$1"></a>\`$1\`<a href="#${wcName}" style="float: right; font-size: 1rem; font-weight: 100;">back to top</a>`).
-    replace(/\r\n|\r|\n\|\s`([a-zA-Z]*)`/g, '\r\n| [$1](#$1)').
-    replace(/\| \[\]\(#\)/g, "");
+  result = result
+    .replace(/\r\n|\r|\n####\s`([a-zA-Z]*)`/g, `\r\n#### <a name="$1"></a>\`$1\`<a href="#${wcName}" style="float: right; font-size: 1rem; font-weight: 100;">back to top</a>`)
+    .replace(/\r\n|\r|\n\|\s`([a-zA-Z]*)`/g, '\r\n| [$1](#$1)')
+    .replace(/\| \[\]\(#\)/g, "");
 
   fs.writeFileSync(destination, result, { encoding: 'utf8'});
 
-  fs.readFile('./demo/apiExamples.md', 'utf8', (err, data) => {
+  fs.readFile('./demo/apiExamples.md', 'utf8', function(err, data) {
     formatTemplateFileContents(data, './demo/apiExamples.md');
   });
 }
 
 /**
- * Compiles `./docTemplates/README.md` -> `./README.md`.
+ * Compiles `./docTemplates/README.md` -> `./README.md`
  */
 
-/**
- *
- */
 function processReadme() {
   const callback = function(updatedContent, outputConfig) {
 
     if (fs.existsSync('./README.md')) {
-      fs.readFile('./README.md', 'utf8', (err, data) => {
+      fs.readFile('./README.md', 'utf8', function(err, data) {
         formatTemplateFileContents(data, './README.md');
       });
     } else {
@@ -124,16 +112,13 @@ function processReadme() {
 }
 
 /**
- * Compiles `./docTemplates/demo.md` -> `./demo/demo.md`.
+ * Compiles `./docTemplates/demo.md` -> `./demo/demo.md`
  */
 
-/**
- *
- */
 function processDemo() {
   const callback = function(updatedContent, outputConfig) {
     if (fs.existsSync('./demo/demo.md')) {
-      fs.readFile('./demo/demo.md', 'utf8', (err, data) => {
+      fs.readFile('./demo/demo.md', 'utf8', function(err, data) {
         formatTemplateFileContents(data, './demo/demo.md');
       });
     } else {
@@ -152,16 +137,13 @@ function processDemo() {
 }
 
 /**
- * Compiles `./docTemplates/apiExamples.md` -> `./demo/apiExamples.md`.
+ * Compiles `./docTemplates/apiExamples.md` -> `./demo/apiExamples.md`
  */
 
-/**
- *
- */
 function processApiExamples() {
   const callback = function(updatedContent, outputConfig) {
     if (fs.existsSync('./demo/apiExamples.md')) {
-      fs.readFile('./demo/apiExamples.md', 'utf8', (err, data) => {
+      fs.readFile('./demo/apiExamples.md', 'utf8', function(err, data) {
         formatApiTableContents(data, './demo/apiExamples.md');
       });
     } else {
@@ -180,28 +162,25 @@ function processApiExamples() {
 }
 
 /**
- * Copy README.md template from static source.
- */
+ * Copy README.md template from static source
+ * */
 
-/**
- *
- */
 function copyReadmeLocally() {
 
-  if (!fs.existsSync(dirDocTemplates)) {
+  if (!fs.existsSync(dirDocTemplates)){
     fs.mkdirSync(dirDocTemplates);
   }
 
   if (!fs.existsSync(readmeFilePath)) {
-    fs.writeFile(readmeFilePath, '', (err) => {
-      if (err) {
+    fs.writeFile(readmeFilePath, '', function(err) {
+      if(err) {
         console.log('ERROR: Unable to create README.md file.', err);
       }
     });
   }
 
-  https.get(readmeTemplateUrl, (response) => {
-    const writeTemplate = response.pipe(fs.createWriteStream(readmeFilePath));
+  https.get(readmeTemplateUrl, function(response) {
+    let writeTemplate = response.pipe(fs.createWriteStream(readmeFilePath));
 
     writeTemplate.on('finish', () => {
       processReadme();
@@ -213,7 +192,7 @@ function copyReadmeLocally() {
 }
 
 /**
- * Run all the actual document generation.
+ * Run all the actual document generation
  */
 copyReadmeLocally();
 processApiExamples();
