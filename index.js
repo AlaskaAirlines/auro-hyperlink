0b18a9f5e74f6878551d73c51f73d055978ba703
Eunsun Mota <eunsun.mota@alaskaair.com>
2024-10-15 11:48:50 -0700

    feat(api): add register static method
    AuroHyperlink.register is to easily register the element without extra importing
    `import "@aurodesignsystem/auro-hyperlink"` will still register this element to <auro-hyperlink>
    `import { AuroHyperlink } from '../src/auro-hyperlink.js` wont register this element until AuroHyperlink.register gets called

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
 import { AuroHyperlink } from './src/auro-hyperlink.js';
-import * as RuntimeUtils from '@aurodesignsystem/auro-library/scripts/utils/runtimeUtils.mjs';
 
-RuntimeUtils.default.prototype.registerComponent('auro-hyperlink', AuroHyperlink);
+AuroHyperlink.register();
