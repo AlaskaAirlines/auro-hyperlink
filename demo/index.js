bdcd2b2d1101525ebafbc1f9fb35b7e04f719b12
Jason Baker <jason@capsule42.com>
2024-10-22 18:58:22 -0600

    refactor: cleanup

--- demo/index.js
+++ demo/index.js
@@ -1,6 +1,7 @@
 import { roleButtonExample } from "../apiExamples/roleButton";
 import { AuroHyperlink } from '../src/auro-hyperlink.js';
 
+AuroHyperlink.register();
 AuroHyperlink.register('custom-hyperlink');
 
 export function initHyperlinkIndexExamples(initCount) {
