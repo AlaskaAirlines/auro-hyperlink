07968fe04b082e88e5352a74546dd55d3b915eec
Eunsun Mota <eunsun.mota@alaskaair.com>
2025-09-25 18:48:02 -0400

    fix: support Enter press on role="button" #302

--- apiExamples/roleButton.js
+++ apiExamples/roleButton.js
@@ -6,22 +6,7 @@ export function roleButtonExample() {
     alert(`You clicked the role button!`);
   });
 
-  roleButton.addEventListener('keydown', handleKeyPress);
-
   roleButtonOndark.addEventListener('click', () => {
     alert(`You clicked the role button!`);
   });
-
-  roleButtonOndark.addEventListener('keydown', handleKeyPress);
-}
-
-function handleKeyPress(event) {
-  if(event.key === 'Enter') {
-    event.preventDefault();
-    alert('Win for a11y! The return key submits!')
-  }
-  else if(event.key === ' ') {
-    event.preventDefault();
-    alert('Win for a11y! The spacebar submits!')
-  }
 }
